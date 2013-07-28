e4ee278e33f9f261036a17da485eee74d1296a13
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2013-07-27 21:47:28 -0400

    [SERVER] Agregada la libreria php-websocket para la programación del servidor

---
+++ server/lib/WebSocket/Application/PhitanaApplication.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace WebSocket\Application;
+
+/**
+ * Websocket-Server tic-tac-toe.
+ *
+ * @author Carlos E. Caballero B. <cijkb.j@gmail.com>
+ * @author Vladimir Cespedes L. <vladwelt@gmail.com>
+ */
+class PhitanaApplication extends Application
+{
+    public function onConnect($connection) {
+        
+    }
+
+    public function onData($data, $client) {
+        
+    }
+
+    public function onDisconnect($connection) {
+        
+    }    
+}
---
+++ server/server.php
@@ -0,0 +1,29 @@
+<?php
+
+// Parse without sections
+$config = parse_ini_file(__DIR__ . '/config/config.ini');
+
+ini_set('display_errors', $config['display_errors']);
+error_reporting(E_ALL);
+
+require(__DIR__ . '/lib/SplClassLoader.php');
+
+$classLoader = new SplClassLoader('WebSocket', __DIR__ . '/lib');
+$classLoader->register();
+
+$server = new \WebSocket\Server(
+    $config['host'], $config['port'], $config['ssl']);
+
+// server settings:
+$server->setMaxClients($config['max_clients']);
+$server->setCheckOrigin($config['check_origin']);
+//$server->setAllowedOrigin('foo.lh');
+$server->setMaxConnectionsPerIp($config['max_connections']);
+$server->setMaxRequestsPerMinute($config['max_request']);
+
+foreach ($config['apps'] as $app) {
+    $_app = '\\WebSocket\\Application\\' . ucfirst($app) . 'Application';
+    $server->registerApplication($app, $_app::getInstance());
+}
+
+$server->run();
