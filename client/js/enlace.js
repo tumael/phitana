d326b3fe543e20aa7747bc013299ed8c4cff422f
vladwelt <vladwelt@gmail.com>
2013-08-01 22:50:10 +0000

    [JS] added javascript tictactoe

--- client/js/enlace.js
+++ client/js/enlace.js
@@ -1,25 +1,42 @@
 var enlace = {
-
+    data : null,
+    action : null,
+    nick : null,
     direccion : 'ws://10.0.0.8:8000/chat',
     socket : null,
-    init: function (info){
+    init: function (msg){
 
         try{
-            socket = new WebSocket(enlace.direccion);
-            socket.onopen    = enlace.detenerConexion;
-            socket.onmessage = enlace.procesarMensaje;
-            socket.onclose   = enlace.iniciarConexion;
+            enlace.socket = new WebSocket(enlace.direccion);
+            enlace.socket.onopen    = enlace.iniciarConexion;
+            enlace.socket.onmessage = enlace.procesarMensaje;
+            enlace.socket.onclose   = enlace.detenerConexion;
         }catch( error){
             console.log(error);
         }
     },
-    iniciarConexion: function(info){
+    iniciarConexion: function(msg){
+        enlace.nick= prompt("Please enter your name:","Your name")
+        enlace.format("user-nick",enlace.nick)
+        console.log("entro men")
+    },
+    detenerConexion : function(msg){
         
     },
-    detenerConexion: function(info){
-
+    procesarMensaje : function(msg){
+        enlace.data=JSON.parse(msg.data)
+        console.log(enlace.data)
     },
-    procesarMensaje:function(info){
-
-    },
+    format : function(action,data){
+        payload=new Object()
+        payload.action=action
+        payload.data=data
+        enlace.socket.send(JSON.stringify(payload))
+    }
 };
+var userlist={
+    userlist : null,
+    init : function(){
+        enlace.format("users-list","")
+    },
+} 
